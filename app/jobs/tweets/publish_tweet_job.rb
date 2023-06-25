3091563c70acf1adb042a8d40007e436c260cbca
Leonid Medovyy <leonid@storypro.io>
2023-06-25 08:44:38 -0700

    adjusting tweet trim to fix the ayrshare errors

--- app/jobs/tweets/publish_tweet_job.rb
+++ app/jobs/tweets/publish_tweet_job.rb
@@ -30,7 +30,7 @@ class Tweets::PublishTweetJob < ApplicationJob
 
     # Truncate tweet_text to fit within the MAX_CHARACTERS limit
     # 28 characters are reserved for URL and a space
-    truncated_tweet_text = tweet_text.truncate(max_characters - 28, omission: '...')
+    truncated_tweet_text = tweet_text.truncate(max_characters - 31, omission: '...')
 
     card_image =  tweet.discussion.story.imaginations.where(aspect_ratio: :card).sample(1)
     card_image_url = "https://ucarecdn.com/#{card_image.last.uploadcare.last['uuid']}/-/format/auto/-/quality/smart/-/preview/"
